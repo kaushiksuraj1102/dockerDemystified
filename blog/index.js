44623b4f03ce85e19287bdaececf25bc22e81296
Suraj Kaushik <surajkaushik@ibm.com>
2024-08-06 09:38:33 +0530

    blog

---
+++ blog/index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log("Hello World");
+console.log(chalk.green.inverse.bold("Success!"));
+console.log(chalk.red.italic.bold("Failure!"));
+console.log(chalk.blue.bold("Pass!"));
